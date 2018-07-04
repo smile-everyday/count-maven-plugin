2849b086b5633874701c4b7e4db7907070561df0
black <1312603070@qq.com>
2018-07-04 15:38:42 +0800

    功能完善

--- src/main/java/cn/lwj/Count.java
+++ src/main/java/cn/lwj/Count.java
@@ -12,34 +12,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @Title:
- * @Author: 李维健
+ * @Title: maven 统计项目下各文件类型数量
+ * @Author: Dark Souls
  * @Date: 2018\7\3 0003
  */
 @Mojo(name = "count", defaultPhase = LifecyclePhase.COMPILE)
-public class Count extends AbstractMojo {
+public class Count extends AbstractMojo{
 
     // 扫描路径
     @Parameter
-    private String path;
+    private String route;
 
     // 需要统计的文件后缀
     @Parameter
     private List<String> suffexs = new ArrayList<>();
 
+    /**
+     * 存储文件名
+     */
+    private List<String> fileNames = new ArrayList<>();
+
+    /**
+     * 文件个数
+     */
+    private int count = 0;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        List<String> fileNames = scanFiles(path);
-        int count = 0;
-        if (null == null || fileNames.equals("")) {
-            System.out.println("没有文件");
+        scanFiles(route);
+
+        if (null == fileNames || fileNames.size() == 0) {
+            System.out.println("No files!!!");
         } else {
             for (String suffex : suffexs) {
                 for (int i = 0; i < fileNames.size(); i++) {
                     if (fileNames.get(i).endsWith(suffex)) {
                         count++;
                     }
-                    if (i == fileNames.size()) {
+                    if (i == fileNames.size() - 1) {
                         System.out.println(suffex + "：" + count);
                         count = 0;
                     }
@@ -48,9 +58,7 @@ public class Count extends AbstractMojo {
         }
     }
 
-    private List<String> scanFiles(String path) {
-        List<String> fileNames = new ArrayList<>();
-
+    private void scanFiles(String path) {
         File file = new File(path);
         if (file.isDirectory()) {
             File[] files = file.listFiles();
@@ -63,6 +71,6 @@ public class Count extends AbstractMojo {
             }
         }
 
-        return fileNames;
     }
+
 }
